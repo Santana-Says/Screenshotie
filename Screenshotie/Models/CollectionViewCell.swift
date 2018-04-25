4bf8d0e1e912c664bf17f020e4785690a8af530a
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-04-25 00:58:52 -0400

    simple welcome screen, implementated photo library

---
+++ Screenshotie/Models/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Screenshotie
+//
+//  Created by Jeffrey Santana on 4/24/18.
+//  Copyright © 2018 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
